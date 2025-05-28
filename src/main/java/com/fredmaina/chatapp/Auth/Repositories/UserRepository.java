6bb23af1197c95778de7fec93ddfaa8bbe432c7b
Fredrick Maina <fchege04@gmail.com>
2025-05-28 12:41:23 +0300

    Create A UserRepository

---
+++ src/main/java/com/fredmaina/chatapp/Auth/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.fredmaina.chatapp.Auth.Repositories;
+
+import com.fredmaina.chatapp.Auth.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);  // Checks if a user with this username exists
+    boolean existsByEmail(String email);
+}
