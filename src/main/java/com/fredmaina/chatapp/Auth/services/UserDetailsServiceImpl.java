22652489dd446d2380cea0b35751ffce0525091d
Fredrick Maina <fchege04@gmail.com>
2025-05-28 12:41:07 +0300

    Implement UserDetail

---
+++ src/main/java/com/fredmaina/chatapp/Auth/services/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.fredmaina.chatapp.Auth.services;
+
+import com.fredmaina.chatapp.Auth.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+        @Autowired
+        private UserRepository userRepository;
+
+        @Override
+        public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+            Optional <com.fredmaina.chatapp.Auth.Models.User> userOptional = userRepository.findByEmail(email);
+            if (userOptional.isEmpty()) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            com.fredmaina.chatapp.Auth.Models.User user = userOptional.get();
+
+            return User.builder()
+                    .disabled(!user.isVerified())
+                    .username(user.getEmail())
+                    .password(user.getPassword())
+                    .roles(String.valueOf(user.getRole()))
+                    .build();
+        }
+    }
