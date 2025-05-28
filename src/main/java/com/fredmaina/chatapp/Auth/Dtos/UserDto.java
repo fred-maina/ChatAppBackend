99ce405e865d0d005b4648bd8f387574102fe4f2
Fredrick Maina <fchege04@gmail.com>
2025-05-28 12:41:13 +0300

    Create A UserDto

---
+++ src/main/java/com/fredmaina/chatapp/Auth/Dtos/UserDto.java
@@ -0,0 +1,19 @@
+package com.fredmaina.chatapp.Auth.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDto {
+    private UUID userId;
+    private String firstName;
+    private String lastName ;
+    private String email ;
+    private String username ;
+}
