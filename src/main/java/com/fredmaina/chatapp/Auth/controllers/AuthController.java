e9e174bd6f0d5308ab65b82cf803d8f58888ee09
Fredrick Maina <fchege04@gmail.com>
2025-05-29 16:20:31 +0300

    Add Google Oauth2 support

--- src/main/java/com/fredmaina/chatapp/Auth/controllers/AuthController.java
+++ src/main/java/com/fredmaina/chatapp/Auth/controllers/AuthController.java
@@ -1,24 +1,29 @@
 package com.fredmaina.chatapp.Auth.controllers;
 
 
-import com.fredmaina.chatapp.Auth.Dtos.AuthResponse;
-import com.fredmaina.chatapp.Auth.Dtos.LoginRequest;
-import com.fredmaina.chatapp.Auth.Dtos.SignUpRequest;
+import com.fredmaina.chatapp.Auth.Dtos.*;
+import com.fredmaina.chatapp.Auth.Models.User;
+import com.fredmaina.chatapp.Auth.Repositories.UserRepository;
 import com.fredmaina.chatapp.Auth.services.AuthService;
+import com.fredmaina.chatapp.Auth.services.JWTService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
-
+@Slf4j
 @Controller
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     AuthService authService;
+    @Autowired
+    JWTService jwtService;
+    @Autowired
+    UserRepository userRepository;
 
 
     @PostMapping("/login")
@@ -39,4 +44,24 @@ public class AuthController {
         }
         return ResponseEntity.status(500).body(authResponse);
     }
+    @PostMapping("/oauth/google")
+    public ResponseEntity<?> googleOAuth(@RequestBody GoogleOAuthRequest request) {
+        AuthResponse response = authService.handleGoogleOAuth(request.getCode(), request.getRedirectUri());
+        log.error(response.toString());
+        if (response.isSuccess()) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(@RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        String email = jwtService.getUsernameFromToken(token);
+        User user = userRepository.findByEmail(email).orElseThrow();
+        return ResponseEntity.ok(new UserDto(user));
+    }
+
+
 }
