4cac707301dc32464815496c915f27f7b47af164
Fredrick Maina <fchege04@gmail.com>
2025-05-29 16:21:48 +0300

    Add UserDto constructor

--- src/main/java/com/fredmaina/chatapp/Auth/Dtos/UserDto.java
+++ src/main/java/com/fredmaina/chatapp/Auth/Dtos/UserDto.java
@@ -1,11 +1,12 @@
 package com.fredmaina.chatapp.Auth.Dtos;
 
+import com.fredmaina.chatapp.Auth.Models.Role;
+import com.fredmaina.chatapp.Auth.Models.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
-
 
 @Getter
 @Setter
@@ -13,7 +14,17 @@ import java.util.UUID;
 public class UserDto {
     private UUID userId;
     private String firstName;
-    private String lastName ;
-    private String email ;
-    private String username ;
+    private String lastName;
+    private String email;
+    private String username;
+    private Role role;
+
+    public UserDto(User user) {
+        this.userId = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.username = user.getUsername();
+        this.role = user.getRole();
+    }
 }
