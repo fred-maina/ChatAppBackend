9c6ade4ab04618c4ff987019a1089a702adde29e
Fredrick Maina <fchege04@gmail.com>
2025-05-31 22:34:35 +0300

    Create Websocket Payload DTO

---
+++ src/main/java/com/fredmaina/chatapp/core/DTOs/WebSocketMessagePayload.java
@@ -0,0 +1,26 @@
+package com.fredmaina.chatapp.core.DTOs;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class WebSocketMessagePayload {
+    private MessageType type; // e.g., "ANON_TO_USER" or "USER_TO_ANON"
+    private String from; // username or sessionId
+    private String to;   // username or sessionId
+    private String content;
+    private String nickname;
+    private String timestamp;
+
+
+}
