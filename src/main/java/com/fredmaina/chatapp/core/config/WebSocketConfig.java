9e171ad447898f0219d9972a65c89fdf64c013b0
Fredrick Maina <fchege04@gmail.com>
2025-05-31 22:34:24 +0300

    Create Websocket config

--- src/main/java/com/fredmaina/chatapp/core/config/WebSocketConfig.java
+++ src/main/java/com/fredmaina/chatapp/core/config/WebSocketConfig.java
@@ -1,26 +1,21 @@
-package com.fredmaina.chatapp.core.config;
-
+package com.fredmaina.chatapp.core.config;// Example for WebSocketConfig.java if choosing TextWebSocketHandler
+import com.fredmaina.chatapp.core.Controllers.ChatWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.beans.factory.annotation.Autowired; // Autowire your handler
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private ChatWebSocketHandler chatWebSocketHandler; // Your existing handler
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/ws/chat")
+                .setAllowedOriginPatterns("*"); // Add .withSockJS() if still needed
     }
 }
-
