de19ee2690f6201d294c28ce4a23937e179ed60a
Fredrick Maina <fchege04@gmail.com>
2025-06-03 22:53:50 +0300

    fix local time date to handle timezones better

--- src/main/java/com/fredmaina/chatapp/core/DTOs/ChatMessageMapper.java
+++ src/main/java/com/fredmaina/chatapp/core/DTOs/ChatMessageMapper.java
@@ -3,18 +3,22 @@ package com.fredmaina.chatapp.core.DTOs;
 import com.fredmaina.chatapp.core.models.ChatMessage;
 
 import java.util.UUID;
-
+import java.time.format.DateTimeFormatter;
 public class ChatMessageMapper {
-        public static ChatMessageDto toDto(ChatMessage msg, UUID currentUserId) {
-            boolean isFromCurrentUser = msg.getFromUser() != null && msg.getFromUser().getId().equals(currentUserId);
+    public static ChatMessageDto toDto(ChatMessage msg, UUID currentUserId) {
+        boolean isFromCurrentUser = msg.getFromUser() != null && msg.getFromUser().getId().equals(currentUserId);
 
-            return ChatMessageDto.builder()
-                    .id(msg.getMessageId().toString())
-                    .text(msg.getContent())
-                    .senderType(isFromCurrentUser ? "self" : "anonymous")
-                    .timestamp(msg.getTimestamp().toString())
-                    .nickname(isFromCurrentUser ? null : msg.getNickname())
-                    .build();
-        }
+        String formattedTimestamp = msg.getTimestamp() != null ?
+                DateTimeFormatter.ISO_INSTANT.format(msg.getTimestamp()) :
+                null;
+
+        return ChatMessageDto.builder()
+                .id(msg.getMessageId().toString())
+                .text(msg.getContent())
+                .senderType(isFromCurrentUser ? "self" : "anonymous")
+                .timestamp(formattedTimestamp) // Use formattedTimestamp
+                .nickname(isFromCurrentUser ? null : msg.getNickname())
+                .build();
+    }
 
 }
