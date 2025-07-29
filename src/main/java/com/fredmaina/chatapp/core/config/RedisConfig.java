1046e854b752d4ae49a84361de47cf441ed9fc09
Fredrick Maina <fchege04@gmail.com>
2025-07-30 00:01:31 +0300

    Add caching

--- src/main/java/com/fredmaina/chatapp/core/config/RedisConfig.java
+++ src/main/java/com/fredmaina/chatapp/core/config/RedisConfig.java
@@ -2,14 +2,19 @@ package com.fredmaina.chatapp.core.config;
 
 
 import com.fredmaina.chatapp.core.Services.RedisMessageSubscriber;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.listener.*;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -39,5 +44,15 @@ public class RedisConfig {
         template.setHashValueSerializer(stringSerializer);
 
         return template;
+    }
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))  // optional TTL
+                .disableCachingNullValues();       // avoid caching null
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
